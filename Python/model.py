223cac0f3c5df04fd36caf6532927123db8e6da1
Eray-hgs <e.oeztoprak@hgs-singen.online>
2025-10-21 09:00:43 +0000

    Pydentic

---
+++ Python/model.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, Field, field_validator, ValidationError
+
+class Spieler(BaseModel):
+    name        : str = Field(default = "Name Vergessen")
+    jahrgang    : int = Field(default =2000, gt=1980,lt=2007)
+    staerke     : int = Field(default =5, gt=0,lt=11)
+    torschuss   : int = Field(default =5, gt=0,lt=11)
+    motivation  : int = Field(default =5, gt=0,lt=11)
+
+    model_config = {"validate_assignment": True}
+
+s = Spieler(name="Lukas",jahrgang=2006,staerke=10,torschuss=10,motivation=1)
+print(s)
+print(s.model_dump())
