4641a464e7c98e73de312dddd837e4b094efdfcb
Eray-hgs <e.oeztoprak@hgs-singen.online>
2025-10-24 07:23:06 +0000

    server

--- Python/model.py
+++ Python/model.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field, field_validator, ValidationError
 
 class Spieler(BaseModel):
     name        : str = Field(default = "Name Vergessen")
-    jahrgang    : int = Field(default =2000, gt=1980,lt=2007)
+    jahrgang    : int = Field(default =2000, gt=1980,le=2007)
     staerke     : int = Field(default =5, gt=0,lt=11)
     torschuss   : int = Field(default =5, gt=0,lt=11)
     motivation  : int = Field(default =5, gt=0,lt=11)
--- Python/server.py
+++ Python/server.py
@@ -1,26 +1,31 @@
 from flask import Flask, request, jsonify
 from pydantic import BaseModel, Field, ValidationError
 from model import Spieler 
+import json
+import os
 
-spielerliste = []
-def attribute():
-   return f"""
-       methode: {request.method}
-       args: {request.args}
-       form: {request.form}
-       data: {request.data}
-       headers: {request.headers}
-       cookies: {request.cookies}
-       path: {request.path}
-       url: {request.url}
-       remote_addr: {request.remote_addr}
-    """
+dateiname = "spieler.json"
+
+if os.path.exists(dateiname):
+    with open(dateiname, "r", encoding="utf-8") as f:
+        daten = json.load(f)
+    spieler_liste = [Spieler(**s) for s in daten]
+    print("📂 Geladene Spieler:")
+    for s in spieler_liste:
+        print(s)
+else:
+    print(f"⚠️ Datei '{dateiname}' wurde nicht gefunden.")
+    print("Es wird eine leere Spielerliste erstellt.")
+    spieler_liste = []
+
 
 app = Flask(__name__)
 
+spieler_liste =[]
 # Route für die Hauptseite
 @app.route('/')
 def home():
+    
     response_attributes = attribute()
     return jsonify({"attributes": response_attributes})
 
@@ -44,12 +49,17 @@ def handle_spieler():
     """Erstellt einen Spieler aus JSON-Daten und prüft es mit Pydantic."""
     try:
         data = request.get_json()
-        spieler = Spieler(**data)
-        spielerliste.append(spieler)
+        s = Spieler(**data)
+
+        spieler_liste.append(s)
+        with open("spieler.json", "w", encoding="utf-8") as f:
+            json.dump([s.model_dump() for s in spieler_liste], f, ensure_ascii=False, indent=4)
+        print("✅ Spieler wurden in 'spieler.json' gespeichert")
+
         return jsonify({
             "status": "ok",
             "message": "Spieler erfolgreich erstellt",
-            "spieler": spieler.model_dump()
+            "spieler": s.model_dump()
         }), 201
     except ValidationError as e:
         return jsonify({
@@ -58,6 +68,26 @@ def handle_spieler():
             "details": e.errors()
         }), 400
 
+@app.route('/test')
+def Test_Ausgabe():
+    daten = ""
+    for s in spieler_liste:
+        daten = (f"{daten} <br> {s}")
+    print (daten)
+    return (f"{daten}")
+
+def attribute():
+   return f"""
+       methode: {request.method}
+       args: {request.args}
+       form: {request.form}
+       data: {request.data}
+       headers: {request.headers}
+       cookies: {request.cookies}
+       path: {request.path}
+       url: {request.url}
+       remote_addr: {request.remote_addr}
+    """
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=12345)  # Server starten
